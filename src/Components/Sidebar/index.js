dfa53010c40fef288877dd8a4c818696b66b571c
paulfarwell <paul@farwell-consultants.com>
2022-09-17 16:02:27 +0300

    home icons

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,13 +1,24 @@
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import './index.scss';
 import LogoS from '../../assets/images/logo.png';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () =>(
     <div className="nav-bar">
         <Link className='logo' to='/'>
              <img src={LogoS} alt="Logo" />
         </Link>
+        <NavLink exact="true" activeclassname="active" to="/">
+         <FontAwesomeIcon icon={faHome} color="#fff" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+         <FontAwesomeIcon icon={faUser} color="#fff" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+         <FontAwesomeIcon icon={faEnvelope} color="#fff" />
+        </NavLink>
+        
     </div>
 )
 export default Sidebar
